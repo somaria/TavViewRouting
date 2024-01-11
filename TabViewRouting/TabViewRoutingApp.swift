4dabc87a51ac944ed8b9d68421f0a0ae32589fb9
XJPIT <138988193+xjpit@users.noreply.github.com>
2024-01-11 16:20:53 +0800

    Initial Commit

---
+++ TabViewRouting/TabViewRoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewRoutingApp.swift
+//  TabViewRouting
+//
+//  Created by daryl on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewRoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
